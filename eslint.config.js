9af099416f4ae84d885ecda2aaafe5347502711f
MalicknND <ndiayemalick062@gmail.com>
2026-08-29 12:46:04 +0200

    fix(eslint): update ignore patterns in ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,21 @@ import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["dist", ".output", ".vinxi"] },
+  {
+    ignores: [
+      "node_modules",
+      "dist",
+      "build",
+      "coverage",
+      ".vercel",
+      ".output",
+      ".vinxi",
+      ".nitro",
+      ".tanstack",
+      ".wrangler",
+      "src/routeTree.gen.ts",
+    ],
+  },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
